5e9881c1051cdfb9cc2f5f9453ee177209c67318
fikrirazor <fikrirazor@outlook.co.id>
2019-11-20 08:50:13 +0700

    update main button

--- app/src/main/java/com/example/greatsuperapp/MainActivity.java
+++ app/src/main/java/com/example/greatsuperapp/MainActivity.java
@@ -10,14 +10,17 @@ import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button BtnPendapatan, BtnLaporan;
+    Button BtnPendapatan, BtnPengeluaran, BtnLaporan, BtnMaps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         BtnPendapatan = findViewById(R.id.btnpendapatan);
+        BtnPengeluaran = findViewById(R.id.btnpengeluaran);
         BtnLaporan = findViewById(R.id.btnlaporan);
+        BtnMaps = findViewById(R.id.btnmaps);
+
         BtnPendapatan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,10 +28,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        BtnPengeluaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, pengeluaran_input_form.class));
+            }
+        });
+
         BtnLaporan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, laporan_view_form.class));
+            }
+        });
+
+        BtnMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, maps_view.class));
             }
         });
     }
--- app/src/main/java/com/example/greatsuperapp/pengeluaran_input_form.java
+++ app/src/main/java/com/example/greatsuperapp/pengeluaran_input_form.java
@@ -1,14 +1,47 @@
 package com.example.greatsuperapp;
+import android.database.Cursor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class pengeluaran_input_form extends AppCompatActivity {
+    protected Cursor cursor;
+    DatabaseHelper dbHelper;
+    Button btnsimpan;
+    EditText textfaktor, texttgl, textjumlah, textketerangan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pengeluaran_input_form);
+
+        dbHelper = new DatabaseHelper(this);
+        textfaktor = (EditText) findViewById(R.id.inputfaktorpengeluaran);
+        texttgl = (EditText) findViewById(R.id.inputtanggalpengeluaran);
+        textjumlah = (EditText) findViewById(R.id.inputjumlahpengeluaran);
+        textketerangan = (EditText) findViewById(R.id.inputketeranganpengeluaran);
+        btnsimpan = (Button) findViewById(R.id.btnsimpanpengeluaran);
+
+        btnsimpan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("insert into pengeluaran(no,factor,tanggal,jumlah,keterangan) values('" +
+                        textfaktor.getText().toString() + "','"+
+                        texttgl.getText().toString() + "','"+
+                        textjumlah.getText().toString() + "','"+
+                        textketerangan.getText().toString() + "')");
+                Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+
     }
 }
