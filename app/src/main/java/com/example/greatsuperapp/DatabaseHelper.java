05653d34f51992f1cb7ab572fa20078e32e00554
fikrirazor <fikrirazor@outlook.co.id>
2019-11-22 01:00:54 +0700

    update

--- app/src/main/java/com/example/greatsuperapp/DatabaseHelper.java
+++ app/src/main/java/com/example/greatsuperapp/DatabaseHelper.java
@@ -17,13 +17,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         String sql = "create table pendapatan(no integer primary key, factor text null, tanggal text null, jumlah integer null, keterangan text null);";
         Log.d("Data","onCreate: " + sql);
         db.execSQL(sql);
-        String sql = "create table pengeluaran(no integer primary key, factor text null, tanggal text null, jumlah integer null, keterangan text null);";
-        Log.d("Data","onCreate: " + sql);
-        db.execSQL(sql);
+        String sql2 = "create table pengeluaran(no integer primary key, factor text null, tanggal text null, jumlah integer null, keterangan text null);";
+        Log.d("Data","onCreate: " + sql2);
+        db.execSQL(sql2);
         sql = "INSERT INTO pendapatan(no,factor,tanggal,jumlah,keterangan) VALUES ('1','tes','2019-11-19','12','oke tes');";
         db.execSQL(sql);
-        sql = "INSERT INTO pengeluaran(no,factor,tanggal,jumlah,keterangan) VALUES ('1','tes','2019-11-19','12','oke tes');";
-        db.execSQL(sql);
+        sql2 = "INSERT INTO pengeluaran(no,factor,tanggal,jumlah,keterangan) VALUES ('1','tes','2019-11-19','12','oke tes');";
+        db.execSQL(sql2);
     }
     @Override
     public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
